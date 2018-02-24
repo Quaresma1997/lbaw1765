e9aeef5ec18ac553fb75db79c4f54c5e8f8eee73
nice5torm <marianadguimaraes@hotmail.com>
2018-02-24 19:10:27 +0000

    segunda pagina semi linda :+1:

--- A3/script.js
+++ A3/script.js
@@ -1,3 +1,13 @@
 /**
  * Created by Mariana on 21/02/2018.
  */
+$(document).ready(function() {
+    $("div.tab-menu>div.list-group>a").click(function(e) {
+        e.preventDefault();
+        $(this).siblings('a.active').removeClass("active");
+        $(this).addClass("active");
+        var index = $(this).index();
+        $("div.tab>div.tab-content").removeClass("active");
+        $("div.tab>div.tab-content").eq(index).addClass("active");
+    });
+});
