db5ad0befbd760275e6aabcdb79e421753bde6bf
nice5torm <marianadguimaraes@hotmail.com>
2018-03-01 21:04:22 +0000

    paginas semi fixes

--- A3/script.js
+++ A3/script.js
@@ -1,13 +1,55 @@
 /**
  * Created by Mariana on 21/02/2018.
  */
+
+/* vertical tab */
+
 $(document).ready(function() {
-    $("div.tab-menu>div.list-group>a").click(function(e) {
+    $("ul.tab-menu>li").click(function(e) {
         e.preventDefault();
-        $(this).siblings('a.active').removeClass("active");
+        $(this).siblings('li.active').removeClass("active");
         $(this).addClass("active");
         var index = $(this).index();
         $("div.tab>div.tab-content").removeClass("active");
         $("div.tab>div.tab-content").eq(index).addClass("active");
+    });
+});
+
+
+/* upload images */
+
+$(document).ready( function() {
+    $(document).on('change', '.btn-file :file', function() {
+        var input = $(this),
+            label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+        input.trigger('fileselect', [label]);
+    });
+
+    $('.btn-file :file').on('fileselect', function(event, label) {
+
+        var input = $(this).parents('.input-group').find(':text'),
+            log = label;
+
+        if( input.length ) {
+            input.val(log);
+        } else {
+            if( log ) alert(log);
+        }
+
+    });
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('#img-upload').attr('src', e.target.result);
+            }
+
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+
+    $("#imgInp").change(function(){
+        readURL(this);
     });
 });
