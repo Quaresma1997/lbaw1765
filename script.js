58bf16df6e18c940257fab379ed65633f9c7a699
mr_base <tiago79c@gmail.com>
2018-03-13 23:28:20 +0000

    DONE

--- script.js
+++ script.js
@@ -15,86 +15,86 @@ $(document).ready(function(){
 });
 
 $(document).on('click', "#btn_editprofile", function (e) {
-    let main_div = $("#user_info_container");
+  let main_div = $("#user_info_container");
 
-    let img = $("#user_info_img");
-  
-    let name = $("#user_info_l1").text();
+  let img = $("#user_info_img");
 
-    let first_name = name.substr(0, name.indexOf(' '));
-    let last_name = name.substr(name.indexOf(' ') + 1, name.size);
+  let name = $("#user_info_l1").text();
 
-    let email = $("#user_info_l2").text();
+  let first_name = name.substr(0, name.indexOf(' '));
+  let last_name = name.substr(name.indexOf(' ') + 1, name.size);
 
-    let location = $("#user_info_l3").text();
+  let email = $("#user_info_l2").text();
 
-    let city = location.substr(0, location.indexOf(','));
-    let country = location.substr(location.indexOf(',') + 2, location.size);
+  let location = $("#user_info_l3").text();
 
-    let btn_img = 
-      "<form action='myform.cgi'>"+
-    "<input type='file' name='fileupload' value='Browse' id='fileupload' class='custom-file - input'>"+
-      "<span class='custom-file-control form-control-file'></span>"
-      "</form>"      
-            
-            ;
-        
-            let div1 =
-      "<div class='row mb-1'>"+
-        "<div class='col-2 col-lg-2 my-auto'>" +
-           "<i class='fas fa-user fa-fw '></i>" +
-        "</div>" +
-        "<div class='col-4 col-lg-5 pl-0'>" +
-           "<input type='text' id='input_first_name' class='form-control' placeholder='First name' value='" + first_name + "'>" +
-        "</div>"+
-        "<div class='col-6 col-lg-5 pl-0'>" +
-            "<input type='text' id='input_last_name' class='form-control' placeholder='Last name' value='" + last_name + "'>" +
-        "</div>"+
-      "</div>"
+  let city = location.substr(0, location.indexOf(','));
+  let country = location.substr(location.indexOf(',') + 2, location.size);
+
+  let btn_img =
+    "<form action='myform.cgi'>" +
+    "<input type='file' name='fileupload' value='Browse' id='fileupload' class='custom-file - input'>" +
+    "<span class='custom-file-control form-control-file'></span>"
+  "</form>"
+
     ;
 
-    let div2 =
-      "<div class='row mb-1'>" +
-      "<div class='col-2 col-lg-2 my-auto'>" +
-      "<i class='fas fa-envelope fa-fw '></i>" +
-      "</div>" +
-      "<div class='col-10 col-lg-10 pr-3 pl-0'>" +
-      "<input type='text' id='input_email' class='form-control' placeholder='First name' value='" + email + "'>" +
-      "</div>" +
+  let div1 =
+    "<div class='row mb-1'>" +
+    "<div class='col-2 my-auto'>" +
+    "<i class='fas fa-user fa-fw '></i>" +
+    "</div>" +
+    "<div class='col-4 px-0'>" +
+    "<input type='text' id='input_first_name' class='form-control' placeholder='First name' value='" + first_name + "'>" +
+    "</div>" +
+    "<div class='col-6 pl-1'>" +
+    "<input type='text' id='input_last_name' class='form-control' placeholder='Last name' value='" + last_name + "'>" +
+    "</div>" +
+    "</div>"
+    ;
 
-      "</div>"
-      ;
+  let div2 =
+    "<div class='row mb-1'>" +
+    "<div class='col-2 my-auto'>" +
+    "<i class='fas fa-envelope fa-fw '></i>" +
+    "</div>" +
+    "<div class='col-10 pr-3 pl-0'>" +
+    "<input type='text' id='input_email' class='form-control' placeholder='First name' value='" + email + "'>" +
+    "</div>" +
 
-
-    let div3 =
-      "<div class='row'>" +
-      "<div class='col-2 col-lg-2 my-auto'>" +
-      "<i class='fas fa-map-marker-alt fa-fw '></i>" +
-      "</div>" +
-      "<div class='col-4 col-lg-4 pr-1 pl-0'>" +
-      "<input type='text' id='input_city' class='form-control' placeholder='City' value='" + city + "'>" +
-      "</div>" +
-      "<div class='col-6 col-lg-6 pr-3 pl-0'>" +
-      "<input type='text' id='input_country' class='form-control' placeholder='Country' value='" + country + "'>" +
-      "</div>" +
-      "</div>"
-      ;
-
-    let btns =
-      "<div class='mt-3'>"+
-          "<button type='button' class='btn btn-success btn-lg btn-block' id='btn_confirm_edit_profile'><span><i class='fas fa-check fa-fw'></i>Confirm</span> </button>" +
-       
-          "<button type='button' class='btn btn-outline-danger btn-lg btn-block' id='btn_cancel_edit_profile'><span><i class='fas fa-times fa-fw'></i>Cancel</span> </button>"+
-      "</div>"
-      ;
+    "</div>"
+    ;
 
 
-    main_div.html(
-      
-      "<img src='./imgs/profile.jpg' id='user_info_img' class='img img-fluid rounded mb-3'>"+
-      btn_img+
-      div1 + div2 + div3 + btns      
-    );
+  let div3 =
+    "<div class='row'>" +
+    "<div class='col-2 my-auto'>" +
+    "<i class='fas fa-map-marker-alt fa-fw '></i>" +
+    "</div>" +
+    "<div class='col-4 px-0'>" +
+    "<input type='text' id='input_city' class='form-control' placeholder='City' value='" + city + "'>" +
+    "</div>" +
+    "<div class='col-6 pl-1'>" +
+    "<input type='text' id='input_country' class='form-control' placeholder='Country' value='" + country + "'>" +
+    "</div>" +
+    "</div>"
+    ;
+
+  let btns =
+    "<div class='mt-3'>" +
+    "<button type='button' class='btn btn-success btn-block' id='btn_confirm_edit_profile'><span><i class='fas fa-check fa-fw'></i> Confirm</span> </button>" +
+
+    "<button type='button' class='btn btn-outline-danger btn-block' id='btn_cancel_edit_profile'><span><i class='fas fa-times fa-fw'></i> Cancel</span> </button>" +
+    "</div>"
+    ;
+
+
+  main_div.html(
+
+    "<img src='./imgs/profile.jpg' id='user_info_img' class='img img-fluid rounded mb-3'>" +
+    btn_img +
+    div1 + div2 + div3 + btns
+  );
 });
 
 
@@ -131,15 +131,15 @@ $(document).on('click', "#btn_confirm_edit_profile", function (e) {
 
   main_div.html(
 
-    "<img src='./imgs/profile.jpg' id='user_info_img' class='img img-fluid rounded mb-3'>" +
-    "<label id='user_info_l1'><i class='fas fa-user fa-fw'></i>" + first_name + " " + last_name + "</label><br>" +
-    "<label id='user_info_l2'><i class='fas fa-envelope fa-fw'></i>" + email + "</label><br>" +
-    "<label id='user_info_l3'><i class='fas fa-map-marker-alt fa-fw'></i>" + city + ", " + country + "</label>" +
+    "<img src='./imgs/profile.jpg' id='user_info_img' class='img img-fluid rounded mb-3'><br>" +
+    "<label id='user_info_l1'><i class='fas fa-user fa-fw mr-1'></i>" + first_name + " " + last_name + "</label><br>" +
+    "<label id='user_info_l2'><i class='fas fa-envelope fa-fw mr-1'></i>" + email + "</label><br>" +
+    "<label id='user_info_l3'><i class='fas fa-map-marker-alt fa-fw mr-1'></i>" + city + ", " + country + "</label>" +
 
-      "<button type='button' class='btn btn-primary btn-lg btn-block mt-3' id='btn_editprofile'>"+
-        "<i class='far fa-edit fa-fw'></i> Edit Profile </button>"+
-      "<button type='button' class='btn btn-outline-danger btn-lg btn-block'>"+
-        "<i class='far fa-trash-alt fa-fw'></i> Delete Profile </button>"
+    "<button type='button' class='btn btn-primary btn-lg btn-block mt-3' id='btn_editprofile'>" +
+    "<i class='far fa-edit fa-fw'></i> Edit Profile </button>" +
+    "<button type='button' class='btn btn-outline-danger btn-lg btn-block'>" +
+    "<i class='far fa-trash-alt fa-fw'></i> Delete Profile </button>"
   );
 
 });
@@ -179,3 +179,92 @@ $(document).on('click', "#btn_cancel_edit_profile", function (e) {
 //   console.log(img_path);
 //   $("#img_text").text(img_path);
 // });
+
+
+function initMap() {
+  // Styles a map in night mode.
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: { lat: 41.178990, lng: -8.597688 },
+    zoom: 16,
+    styles: [
+      { elementType: 'geometry', stylers: [{ color: '#242f3e' }] },
+      { elementType: 'labels.text.stroke', stylers: [{ color: '#242f3e' }] },
+      { elementType: 'labels.text.fill', stylers: [{ color: '#746855' }] },
+      {
+        featureType: 'administrative.locality',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#d59563' }]
+      },
+      {
+        featureType: 'poi',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#d59563' }]
+      },
+      {
+        featureType: 'poi.park',
+        elementType: 'geometry',
+        stylers: [{ color: '#263c3f' }]
+      },
+      {
+        featureType: 'poi.park',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#6b9a76' }]
+      },
+      {
+        featureType: 'road',
+        elementType: 'geometry',
+        stylers: [{ color: '#38414e' }]
+      },
+      {
+        featureType: 'road',
+        elementType: 'geometry.stroke',
+        stylers: [{ color: '#212a37' }]
+      },
+      {
+        featureType: 'road',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#9ca5b3' }]
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'geometry',
+        stylers: [{ color: '#746855' }]
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'geometry.stroke',
+        stylers: [{ color: '#1f2835' }]
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#f3d19c' }]
+      },
+      {
+        featureType: 'transit',
+        elementType: 'geometry',
+        stylers: [{ color: '#2f3948' }]
+      },
+      {
+        featureType: 'transit.station',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#d59563' }]
+      },
+      {
+        featureType: 'water',
+        elementType: 'geometry',
+        stylers: [{ color: '#17263c' }]
+      },
+      {
+        featureType: 'water',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#515c6d' }]
+      },
+      {
+        featureType: 'water',
+        elementType: 'labels.text.stroke',
+        stylers: [{ color: '#17263c' }]
+      }
+    ]
+  });
+}
