ad2eca0c6bb214bf39e2074d1ef781c20a99c0ac
Quaresma1997 <Quaresma1997@users.noreply.github.com>
2018-03-22 11:32:28 +0000

    Add files via upload

--- lbaw1765_db.sql
+++ lbaw1765_db.sql
@@ -1,7 +1,8 @@
 --Types Enums
+PRAGMA foreign_keys = ON;
 
-DROP TYPE IF EXISTS category_type CASCADE;
-CREATE TYPE category_type AS ENUM(
+DROP TYPE IF EXISTS categories CASCADE;
+CREATE TYPE categories AS ENUM(
     'Music',
     'Sports',
     'Entertainment',
@@ -9,43 +10,54 @@ CREATE TYPE category_type AS ENUM(
     'Business',
     'Other'
 );
-DROP TYPE IF EXISTS event_type CASCADE;
-CREATE TYPE event_type AS ENUM(
+DROP TYPE IF EXISTS types_of_event CASCADE;
+CREATE TYPE types_of_event AS ENUM(
     'Public',
     'Private'
 );
 
 --Tables
-DROP TABLE IF EXISTS admin CASCADE;
-CREATE TABLE admin (
+DROP TABLE IF EXISTS admins CASCADE;
+CREATE TABLE admins (
     id SERIAL NOT NULL,
     username text NOT NULL,
     password text NOT NULL,
-    email text NOT NULL
+    email text NOT NULL,
+    CONSTRAINT admins_pk PRIMARY KEY (id),
+    CONSTRAINT admins_email_uk UNIQUE (email)
 );
 
-DROP TABLE IF EXISTS city CASCADE;
-CREATE TABLE city (
+DROP TABLE IF EXISTS cities CASCADE;
+CREATE TABLE cities (
     id SERIAL NOT NULL,
     name text NOT NULL,
-    country_id INTEGER NOT NULL
+    country_id INTEGER NOT NULL,
+    CONSTRAINT cities_pk PRIMARY KEY (id),
+    CONSTRAINT cities_name_uk UNIQUE (name),
+    CONSTRAINT cities_country_id_fk FOREIGN KEY (country_id) REFERENCES 
+    countries(id) ON DELETE SET NULL
 );
 
-DROP TABLE IF EXISTS country CASCADE;
-CREATE TABLE country (
+DROP TABLE IF EXISTS countries CASCADE;
+CREATE TABLE countries (
     id SERIAL NOT NULL,
-    name text NOT NULL
+    name text NOT NULL,
+    CONSTRAINT countries_pk PRIMARY KEY (id),
+    CONSTRAINT countries_name_uk UNIQUE (name)
 );
 
-DROP TABLE IF EXISTS done CASCADE;
-CREATE TABLE done (
+DROP TABLE IF EXISTS dones CASCADE;
+CREATE TABLE dones (
     event_id INTEGER NOT NULL,
     rating INTEGER NOT NULL,
-    CONSTRAINT rating_ck CHECK (((rating > 0) AND (rating <= 5)))
+    CONSTRAINT dones_pk PRIMARY KEY (event_id),
+    CONSTRAINT dones_event_id_fk FOREIGN KEY (event_id) REFERENCES
+     events(id) ON UPDATE CASCADE,
+    CONSTRAINT rating_ck CHECK (((rating >= 1) AND (rating <= 5)))
 );
 
-DROP TABLE IF EXISTS event CASCADE;
-CREATE TABLE event (
+DROP TABLE IF EXISTS events CASCADE;
+CREATE TABLE events (
     id SERIAL NOT NULL,
     name text NOT NULL,
     date TIMESTAMP WITH TIME zone NOT NULL,
@@ -54,250 +66,167 @@ CREATE TABLE event (
     localization_id INTEGER NOT NULL,
     image_id INTEGER NOT NULL,
     type event_type NOT NULL,
-    category category_type NOT NULL,
+    category categories NOT NULL,
+    CONSTRAINT events_pk PRIMARY KEY (id),
+    CONSTRAINT events_owner_id_fk FOREIGN KEY (owner_id) REFERENCES 
+    users(id) ON UPDATE CASCADE,
+    CONSTRAINT events_localization_id_fk FOREIGN KEY (localization_id) REFERENCES 
+    localizations(id) ON DELETE SET NULL,
+    CONSTRAINT events_image_id_fk FOREIGN KEY (image_id) REFERENCES 
+    images(id) ON DELETE SET NULL,
     CONSTRAINT date_ck CHECK ((date > now()))
 );
 
-DROP TABLE IF EXISTS eventInvite CASCADE;
-CREATE TABLE eventInvite (
+DROP TABLE IF EXISTS event_invites CASCADE;
+CREATE TABLE event_invites (
     id SERIAL NOT NULL,
     answer text NOT NULL,
     event_id INTEGER NOT NULL,
     owner_id INTEGER NOT NULL,
-    receiver_id INTEGER NOT NULL
+    receiver_id INTEGER NOT NULL,
+    CONSTRAINT event_invites_pk PRIMARY KEY (id),
+    CONSTRAINT event_invites_event_id_fk FOREIGN KEY (event_id) REFERENCES 
+    not_dones(event_id) ON DELETE CASCADE,
+    CONSTRAINT event_invites_owner_id_fk FOREIGN KEY (owner_id) REFERENCES 
+    owners(id) ON DELETE SET NULL,
+    CONSTRAINT event_invites_receiver_id_fk FOREIGN KEY (receiver_id) REFERENCES 
+    users(id) ON DELETE SET NULL
 );
 
-DROP TABLE IF EXISTS friendActivity CASCADE;
-CREATE TABLE friendActivity (
+DROP TABLE IF EXISTS friend_activities CASCADE;
+CREATE TABLE friend_activities (
     id SERIAL NOT NULL,
     sender_id INTEGER NOT NULL,
     receiver_id INTEGER NOT NULL,
-    event_id INTEGER NOT NULL
+    event_id INTEGER NOT NULL,
+    CONSTRAINT friend_activities_pk PRIMARY KEY (id),
+    CONSTRAINT friend_activities_sender_id_fk FOREIGN KEY (sender_id) REFERENCES
+    users(id) ON DELETE SET NULL,
+    CONSTRAINT friend_activities_receiver_id_fk FOREIGN KEY (receiver_id) REFERENCES 
+    users(id) ON DELETE SET NULL,
+    CONSTRAINT friend_activities_event_id_fk FOREIGN KEY (event_id) REFERENCES 
+    events(id) ON DELETE CASCADE
 );
 
-DROP TABLE IF EXISTS friendRequest CASCADE;
-CREATE TABLE friendRequest (
+DROP TABLE IF EXISTS friend_requests CASCADE;
+CREATE TABLE friend_requests (
     id SERIAL NOT NULL,
     answer text NOT NULL,
     sender_id INTEGER NOT NULL,
     receiver_id INTEGER NOT NULL,
-    event_id INTEGER NOT NULL
+    event_id INTEGER NOT NULL,
+    CONSTRAINT friend_requests_pk PRIMARY KEY (id),
+    CONSTRAINT friend_requests_sender_id_fk FOREIGN KEY (sender_id) REFERENCES 
+    participants(id) ON DELETE CASCADE,
+    CONSTRAINT friend_requests_receiver_id_fk FOREIGN KEY (receiver_id) REFERENCES 
+    users(id) ON DELETE CASCADE
 );
 
-DROP TABLE IF EXISTS image CASCADE;
-CREATE TABLE image (
+DROP TABLE IF EXISTS images CASCADE;
+CREATE TABLE images (
     id SERIAL NOT NULL,
-    path text NOT NULL
+    path text NOT NULL,
+    CONSTRAINT images_pk PRIMARY KEY (id),
+    CONSTRAINT images_path_uk UNIQUE (path)
 );
 
-DROP TABLE IF EXISTS localization CASCADE;
-CREATE TABLE localization (
+DROP TABLE IF EXISTS localizations CASCADE;
+CREATE TABLE localizations (
     id SERIAL NOT NULL,
     name text NOT NULL,
     address text NOT NULL,
     latitude FLOAT,
     longitude FLOAT,
-    city_id INTEGER NOT NULL
+    city_id INTEGER NOT NULL,
+    CONSTRAINT localizations_pk PRIMARY KEY (id),
+    CONSTRAINT localizations_city_id_fk FOREIGN KEY (city_id) REFERENCES 
+    cities(id) ON DELETE SET NULL
 );
 
-DROP TABLE IF EXISTS notDone CASCADE;
-CREATE TABLE notDone (
-    event_id INTEGER NOT NULL
+DROP TABLE IF EXISTS not_dones CASCADE;
+CREATE TABLE not_dones (
+    event_id INTEGER NOT NULL,
+    CONSTRAINT not_dones_pk PRIMARY KEY (event_id),
+    CONSTRAINT not_dones_event_id_fk FOREIGN KEY (event_id) REFERENCES 
+    events(id) ON UPDATE CASCADE
 );
 
-DROP TABLE IF EXISTS option CASCADE;
-CREATE TABLE option (
+DROP TABLE IF EXISTS options CASCADE;
+CREATE TABLE options (
     id SERIAL NOT NULL,
     description text NOT NULL,
-    poll_id INTEGER NOT NULL
+    poll_id INTEGER NOT NULL,
+    CONSTRAINT options_pk PRIMARY KEY (id),
+    CONSTRAINT options_poll_id_fk FOREIGN KEY (poll_id) REFERENCES 
+    polls(id) ON DELETE CASCADE
 );
 
-DROP TABLE IF EXISTS owner CASCADE;
-CREATE TABLE owner (
-    user_id INTEGER NOT NULL,
-    event_id INTEGER NOT NULL
-);
-
-DROP TABLE IF EXISTS participant CASCADE;
-CREATE TABLE participant (
-    user_id INTEGER NOT NULL,
-    event_id INTEGER NOT NULL
-);
-
-DROP TABLE IF EXISTS poll CASCADE;
-CREATE TABLE poll (
+DROP TABLE IF EXISTS owners CASCADE;
+CREATE TABLE owners (
     id SERIAL NOT NULL,
-    post_id INTEGER NOT NULL
+    user_id INTEGER NOT NULL,
+    event_id INTEGER NOT NULL,
+    CONSTRAINT owners_pk PRIMARY KEY (id),
+    CONSTRAINT owners_user_id_event_id_uk UNIQUE (user_id, event_id),
+    CONSTRAINT owners_user_id_fk FOREIGN KEY (user_id) REFERENCES
+    users(id) ON UPDATE CASCADE,
+    CONSTRAINT owners_event_id_fk FOREIGN KEY (event_id) REFERENCES 
+    events(id) ON DELETE SET NULL
 );
 
-DROP TABLE IF EXISTS post CASCADE;
-CREATE TABLE post (
+DROP TABLE IF EXISTS participants CASCADE;
+CREATE TABLE participants (
+    id SERIAL NOT NULL,
+    user_id INTEGER NOT NULL,
+    event_id INTEGER NOT NULL,
+    CONSTRAINT participants_pk PRIMARY KEY (id),
+    CONSTRAINT participants_user_id_event_id_uk UNIQUE (user_id, event_id),
+    CONSTRAINT participants_user_id_fk FOREIGN KEY (user_id) REFERENCES 
+    users(id) ON UPDATE CASCADE,
+    CONSTRAINT participants_event_id_fk FOREIGN KEY (event_id) REFERENCES 
+    events(id) ON DELETE SET NULL
+);
+
+DROP TABLE IF EXISTS polls CASCADE;
+CREATE TABLE polls (
+    id SERIAL NOT NULL,
+    post_id INTEGER NOT NULL,
+    CONSTRAINT polls_pk PRIMARY KEY (id),
+    CONSTRAINT polls_post_id_fk FOREIGN KEY (post_id) REFERENCES 
+    posts(id) ON DELETE CASCADE
+);
+
+DROP TABLE IF EXISTS posts CASCADE;
+CREATE TABLE posts (
     id SERIAL NOT NULL,
     description text NOT NULL,
     date TIMESTAMP WITH TIME zone NOT NULL,
     event_id INTEGER NOT NULL,
     user_id INTEGER NOT NULL,
-    image_id INTEGER NOT NULL
+    image_id INTEGER NOT NULL,
+    CONSTRAINT posts_pk PRIMARY KEY (id),
+    CONSTRAINT posts_event_id_fk FOREIGN KEY (event_id) REFERENCES 
+    events(id) ON DELETE CASCADE,
+    CONSTRAINT posts_user_id_fk FOREIGN KEY (user_id) REFERENCES 
+    users(id) ON DELETE CASCADE,
+    CONSTRAINT posts_image_id_fk FOREIGN KEY (image_id) REFERENCES 
+    images(id) ON DELETE SET NULL
 );
 
-DROP TABLE IF EXISTS "user" CASCADE;
-CREATE TABLE "user" (
+DROP TABLE IF EXISTS users CASCADE;
+CREATE TABLE users (
     id SERIAL NOT NULL,
     username text NOT NULL,
     password text NOT NULL,
     email text NOT NULL,
-    registDate TIMESTAMP WITH TIME zone DEFAULT now() NOT NULL,
-    firstName text NOT NULL,
-    lastName text NOT NULL,
-    imagePath text NOT NULL,
-    city_id INTEGER NOT NULL
+    regist_date TIMESTAMP WITH TIME zone DEFAULT now() NOT NULL,
+    first_name text NOT NULL,
+    last_name text NOT NULL,
+    image_path text NOT NULL,
+    city_id INTEGER NOT NULL,
+    CONSTRAINT users_pk PRIMARY KEY (id),
+    CONSTRAINT users_name_uk UNIQUE (username),
+    CONSTRAINT users_email_uk UNIQUE (email),
+    CONSTRAINT users_city_id_fk FOREIGN KEY (city_id) REFERENCES 
+    cities(id) ON DELETE SET NULL
 );
-
--- Primary Keys and Uniques
-
-ALTER TABLE ONLY admin
-    ADD CONSTRAINT admin_pk PRIMARY KEY (id);
-
-ALTER TABLE ONLY admin
-    ADD CONSTRAINT admin_email_uk UNIQUE (email);
-
-ALTER TABLE ONLY city
-    ADD CONSTRAINT city_pk PRIMARY KEY (id);  
-
-ALTER TABLE ONLY city
-    ADD CONSTRAINT city_name_uk UNIQUE (name);
-
-ALTER TABLE ONLY country
-    ADD CONSTRAINT country_pk PRIMARY KEY (id);  
-
-ALTER TABLE ONLY country
-    ADD CONSTRAINT country_name_uk UNIQUE (name);
-
-ALTER TABLE ONLY done
-    ADD CONSTRAINT done_pk PRIMARY KEY (event_id);  
-
-ALTER TABLE ONLY event
-    ADD CONSTRAINT event_pk PRIMARY KEY (id);  
-
-ALTER TABLE ONLY eventInvite
-    ADD CONSTRAINT eventInvite_pk PRIMARY KEY (id);  
-
-ALTER TABLE ONLY friendActivity
-    ADD CONSTRAINT friendActivity_pk PRIMARY KEY (id);  
-
-ALTER TABLE ONLY friendRequest
-    ADD CONSTRAINT friendRequest_pk PRIMARY KEY (id); 
-
-ALTER TABLE ONLY image
-    ADD CONSTRAINT image_pk PRIMARY KEY (id); 
-
-ALTER TABLE ONLY image
-    ADD CONSTRAINT image_path_uk UNIQUE (path);  
-
-ALTER TABLE ONLY localization
-    ADD CONSTRAINT localization_pk PRIMARY KEY (id);
-
-ALTER TABLE ONLY notDone
-    ADD CONSTRAINT notDone_pk PRIMARY KEY (event_id); 
-
-ALTER TABLE ONLY option
-    ADD CONSTRAINT option_pk PRIMARY KEY (id);
-
-ALTER TABLE ONLY owner
-    ADD CONSTRAINT owner_pk PRIMARY KEY (user_id, event_id); 
-
-ALTER TABLE ONLY participant
-    ADD CONSTRAINT participant_pk PRIMARY KEY (user_id, event_id); 
-
-ALTER TABLE ONLY poll
-    ADD CONSTRAINT poll_pk PRIMARY KEY (id);
-
-ALTER TABLE ONLY post
-    ADD CONSTRAINT post_pk PRIMARY KEY (id);
-
-ALTER TABLE ONLY "user"
-    ADD CONSTRAINT user_pk PRIMARY KEY (id);
-
-ALTER TABLE ONLY "user"
-    ADD CONSTRAINT user_name_uk UNIQUE (username);
-
-ALTER TABLE ONLY "user"
-    ADD CONSTRAINT user_email_uk UNIQUE (email);
-
--- Foreign Keys
-
-ALTER TABLE ONLY city
-    ADD CONSTRAINT city_country_id_fk FOREIGN KEY (country_id) REFERENCES country(id) ON DELETE SET NULL;
-
-ALTER TABLE ONLY done
-    ADD CONSTRAINT done_event_id_fk FOREIGN KEY (event_id) REFERENCES event(id) ON UPDATE CASCADE;
-
-ALTER TABLE ONLY event
-    ADD CONSTRAINT event_owner_id_fk FOREIGN KEY (owner_id) REFERENCES "user"(id) ON UPDATE CASCADE;
-
-ALTER TABLE ONLY event
-    ADD CONSTRAINT event_localization_id_fk FOREIGN KEY (localization_id) REFERENCES localization(id) ON DELETE SET NULL;
-
-ALTER TABLE ONLY event
-    ADD CONSTRAINT event_image_id_fk FOREIGN KEY (image_id) REFERENCES image(id) ON DELETE SET NULL;
-
-ALTER TABLE ONLY eventInvite
-    ADD CONSTRAINT eventInvite_event_id_fk FOREIGN KEY (event_id) REFERENCES notDone(event_id) ON DELETE CASCADE;
-
-ALTER TABLE ONLY eventInvite
-    ADD CONSTRAINT eventInvite_owner_id_fk FOREIGN KEY (owner_id) REFERENCES "user"(id) ON DELETE SET NULL;
-
-ALTER TABLE ONLY eventInvite
-    ADD CONSTRAINT eventInvite_receiver_id_fk FOREIGN KEY (receiver_id) REFERENCES "user"(id) ON DELETE SET NULL;
-
-ALTER TABLE ONLY friendActivity
-    ADD CONSTRAINT friendActivity_sender_id_fk FOREIGN KEY (sender_id) REFERENCES "user"(id) ON DELETE SET NULL;
-
-ALTER TABLE ONLY friendActivity
-    ADD CONSTRAINT friendActivity_receiver_id_fk FOREIGN KEY (receiver_id) REFERENCES "user"(id) ON DELETE SET NULL;
-
-ALTER TABLE ONLY friendActivity
-    ADD CONSTRAINT friendActivity_event_id_fk FOREIGN KEY (event_id) REFERENCES event(id) ON DELETE CASCADE;
-
-ALTER TABLE ONLY friendRequest
-    ADD CONSTRAINT friendRequest_sender_id_fk FOREIGN KEY (sender_id) REFERENCES "user"(id) ON DELETE CASCADE;
-
-ALTER TABLE ONLY friendRequest
-    ADD CONSTRAINT friendRequest_receiver_id_fk FOREIGN KEY (receiver_id) REFERENCES "user"(id) ON DELETE CASCADE;
-
-ALTER TABLE ONLY localization
-    ADD CONSTRAINT localization_city_id_fk FOREIGN KEY (city_id) REFERENCES city(id) ON DELETE SET NULL;
-
-ALTER TABLE ONLY notDone
-    ADD CONSTRAINT notDone_event_id_fk FOREIGN KEY (event_id) REFERENCES event(id) ON UPDATE CASCADE;
-
-ALTER TABLE ONLY option
-    ADD CONSTRAINT option_poll_id_fk FOREIGN KEY (poll_id) REFERENCES poll(id) ON DELETE CASCADE;
-
-ALTER TABLE ONLY owner
-    ADD CONSTRAINT owner_user_id_fk FOREIGN KEY (user_id) REFERENCES "user"(id) ON UPDATE CASCADE;
-
-ALTER TABLE ONLY owner
-    ADD CONSTRAINT owner_event_id_fk FOREIGN KEY (event_id) REFERENCES event(id) ON DELETE SET NULL;
-
-ALTER TABLE ONLY participant
-    ADD CONSTRAINT participant_user_id_fk FOREIGN KEY (user_id) REFERENCES "user"(id) ON UPDATE CASCADE;
-
-ALTER TABLE ONLY participant
-    ADD CONSTRAINT participant_event_id_fk FOREIGN KEY (event_id) REFERENCES event(id) ON DELETE SET NULL;
-
-ALTER TABLE ONLY poll
-    ADD CONSTRAINT poll_post_id_fk FOREIGN KEY (post_id) REFERENCES post(id) ON DELETE CASCADE;
-
-ALTER TABLE ONLY post
-    ADD CONSTRAINT post_event_id_fk FOREIGN KEY (event_id) REFERENCES event(id) ON DELETE CASCADE;
-
-ALTER TABLE ONLY post
-    ADD CONSTRAINT post_user_id_fk FOREIGN KEY (user_id) REFERENCES "user"(id) ON DELETE CASCADE;
-
-ALTER TABLE ONLY post
-    ADD CONSTRAINT post_image_id_fk FOREIGN KEY (image_id) REFERENCES image(id) ON DELETE SET NULL;
-
-ALTER TABLE ONLY "user"
-    ADD CONSTRAINT user_city_id_fk FOREIGN KEY (city_id) REFERENCES city(id) ON DELETE SET NULL;
-
