7c1a924570bcec1da158b1970a3d52bca48cbdba
rpedro10 <rpedro10@gmail.com>
2018-04-02 17:25:08 +0100

    inserts started

---
+++ inserts.sql
@@ -0,0 +1,205 @@
+-- Here goes the SQL code - INSERTS
+ 
+INSERT INTO users (id,username,password,email,regist_date,first_name,last_name, image_path,city_id)
+			VALUES (1,'sodales','GUL95ZXR9EX','sodales.at@curae.co.uk','2018-03-07 22:23:34','Zeph','Griffin','/imgs/natu.jpg');
+			
+			
+INSERT INTO users (id,username,password,email,regist_date,first_name,last_name, image_path,city_id)
+			VALUES (2,'uso1','12345','aliquam.iaculis.lacus@amet.co.uk','2018-04-07 11:23:34','Ben','Warren','/imgs/natur.jpg');
+			
+INSERT INTO users (id,username,password,email,regist_date,first_name,last_name, image_path,city_id)
+			VALUES (3,'robin','pass123','amet.ante@faucibusleo.net','2018-02-12 15:55:12','Robin','Wright','/imgs/natur.jpg');
+			
+			
+INSERT INTO users (id,username,password,email,regist_date,first_name,last_name, image_path,city_id)
+			VALUES (4,'bar123','semper123','ut.dolor@gmail.com','2017-12-12 23:55:12','Barry','Allen','/imgs/natur.jpg');
+			
+INSERT INTO users (id,username,password,email,regist_date,first_name,last_name, image_path,city_id)
+			VALUES (5,'reddevil','LBAW','Nulla@et.net','2018-02-12 15:55:12','Andrew','Irons','/imgs/november.jpg');
+			
+INSERT INTO users (id,username,password,email,regist_date,first_name,last_name, image_path,city_id)
+			VALUES (6,'rpedro10','lbaw1765','rpedro10@iol.pt','2018-01-01 23:55:12','Rui','Araujo','/imgs/fer.jpg');
+			
+						
+INSERT INTO users (id,username,password,email,regist_date,first_name,last_name, image_path,city_id)
+			VALUES (7,'joss123','CKB15AAW5MM','ante@fleo.com','2018-03-12 12:55:12','Joss','Stone','/imgs/natur.jpg');
+			
+INSERT INTO users (id,username,password,email,regist_date,first_name,last_name, image_path,city_id)
+			VALUES (8,'top123','SXT16TTW3MH','cursus.et@orciUt.co.uk','2018-01-03 13:55:12','Chris','Harris','/imgs/natur.jpg');
+			
+			
+INSERT INTO users (id,username,password,email,regist_date,first_name,last_name, image_path,city_id)
+			VALUES (9,'roland1','ZYS24FHN5GR','eget.dictum@orciDonec.edu','2018-03-12 09:55:12','Roland','Schitt','/imgs/natur.jpg');
+						
+INSERT INTO users (id,username,password,email,regist_date,first_name,last_name, image_path,city_id)
+			VALUES (10,'david123','ZUS29FRTGVJ','amet@faucibusleo.net','2018-01-22 15:07:12','David','Rose','/imgs/natur.jpg');
+						
+INSERT INTO users (id,username,password,email,regist_date,first_name,last_name, image_path,city_id)
+			VALUES (11,'catones', 'QQQ8EFHNGNR','amec.donec@faucibusleo.net','2017-11-12 15:55:12','Carlos','Antonio','/imgs/november.jpg');
+						
+INSERT INTO users (id,username,password,email,regist_date,first_name,last_name, image_path,city_id)
+			VALUES (12,'emanem','ASDFGHJKL','donex@sapo.net','2018-01-13 15:55:12','Raheem','Sterling','/imgs/natur.jpg');			
+			
+INSERT INTO users (id,username,password,email,regist_date,first_name,last_name, image_path,city_id)
+			VALUES (13,'ufoExtis','ZXCVBNM','amet@iol.net','2018-02-12 15:55:12','Delle','Alli','/imgs/pyr.jpg');
+						
+INSERT INTO users (id,username,password,email,regist_date,first_name,last_name, image_path,city_id)
+			VALUES (14,'ragnar','QAZWSXEDC','risus.In.mi@egestas.com','2018-02-09 15:55:12','Thor','Ragnarok','/imgs/fer.jpg');
+						
+INSERT INTO users (id,username,password,email,regist_date,first_name,last_name, image_path,city_id)
+			VALUES (15,'seth','QWERTYUIOP','aliquet.diam.Sed@tinciduntnibh.co.uk','2018-01-12 15:55:12','Seth','Byers','/imgs/natu.jpg');
+						
+INSERT INTO users (id,username,password,email,regist_date,first_name,last_name, image_path,city_id)
+			VALUES (16,'edNorton','TYT71DOD7YN','scelerisque.scelerisque.dui@arcuiaculisenim.ca','2018-02-23 15:55:12','Ed','Norton','/imgs/natur.jpg');
+						
+INSERT INTO users (id,username,password,email,regist_date,first_name,last_name, image_path,city_id)
+			VALUES (17,'Pacquiao','SXT16TTW3MH','magnis@cursuset.edu','2018-04-12 15:55:12','Paky','Barret','/imgs/natur.jpg');	
+			
+INSERT INTO users (id,username,password,email,regist_date,first_name,last_name, image_path,city_id)
+			VALUES (18,'steven','MPS10QPK6UE','arcu.Vestibulum@amet.org','2018-01-02 12:55:12','Donovan','Stevenson','/imgs/pyr.jpg');
+						
+INSERT INTO users (id,username,password,email,regist_date,first_name,last_name, image_path,city_id)
+			VALUES (19,'porter123','QIG24ZOK3EM','dui.nec@ultriciesadipiscing.co.uk','2018-02-12 15:55:12','Porter','Osborn','/imgs/natu.jpg');
+						
+INSERT INTO users (id,username,password,email,regist_date,first_name,last_name, image_path,city_id)
+			VALUES (20,'human','ZYG87WQA6FX','facilisis.magna.tellus@sociis.net','2018-04-01 11:55:12','Hu','Randolphe','/imgs/pyr.jpg');
+						
+						
+INSERT INTO events (id,name,date,description,owner_id,localization_id,image_id)
+			VALUES (1,'Antonys Birthday Party','nunc ac mattis ornare, lectus',1,1,1);
+			
+INSERT INTO events (id,name,date,description,owner_id,localization_id,image_id)
+			VALUES (2,'ENEI','Nunc quis arcu vel quam',2,2,2);
+			
+INSERT INTO events (id,name,date,description,owner_id,localization_id,image_id)
+			VALUES (3,'RockInRio','tempus eu, ligula. Aenean euismod',3,3,3);
+			
+INSERT INTO events (id,name,date,description,owner_id,localization_id,image_id)
+			VALUES (4,'Nos Alive','dignissim pharetra. Nam ac nulla.',3,4,4);
+			
+INSERT INTO events (id,name,date,description,owner_id,localization_id,image_id)
+			VALUES (5,'Christmas Dinner','dignissim pharetra. Nam ac nulla.',4,5,5);
+			
+INSERT INTO events (id,name,date,description,owner_id,localization_id,image_id)
+			VALUES (6,'Mark Birthday Party','dignissim pharetra. Nam ac nulla.',1,1,6);
+			
+INSERT INTO events (id,name,date,description,owner_id,localization_id,image_id)
+			VALUES (7,'WebSummit','lorem lorem, luctus ut, pellentesque',6,6,7);
+			
+INSERT INTO events (id,name,date,description,owner_id,localization_id,image_id)
+			VALUES (8,'Ted Talk','sollicitudin orci sem eget massa.',12,2,8);
+			
+			
+			
+INSERT INTO dones (event_id,rating)
+			VALUES (2,5);
+INSERT INTO dones (event_id,rating)
+			VALUES (7,3);
+			
+INSERT INTO not_dones (event_id,)
+			VALUES (1);
+INSERT INTO not_dones (event_id,)
+			VALUES (3);
+INSERT INTO not_dones (event_id,)
+			VALUES (4);
+INSERT INTO not_dones (event_id,)
+			VALUES (5);
+INSERT INTO not_dones (event_id,)
+			VALUES (6);
+INSERT INTO not_dones (event_id,)
+			VALUES (8);
+
+			
+INSERT INTO participants (id,user_id,event_id)
+			VALUES (1,1,1);
+INSERT INTO participants (id,user_id,event_id)
+			VALUES (2,2,1);
+INSERT INTO participants (id,user_id,event_id)
+			VALUES (3,2,2);
+INSERT INTO participants (id,user_id,event_id)
+			VALUES (4,3,1);			
+INSERT INTO participants (id,user_id,event_id)
+			VALUES (5,4,1);			
+INSERT INTO participants (id,user_id,event_id)
+			VALUES (6,5,8);			
+INSERT INTO participants (id,user_id,event_id)
+			VALUES (7,6,8);			
+INSERT INTO participants (id,user_id,event_id)
+			VALUES (8,7,3);			
+INSERT INTO participants (id,user_id,event_id)
+			VALUES (9,12,3);			
+INSERT INTO participants (id,user_id,event_id)
+			VALUES (10,13,3);			
+INSERT INTO participants (id,user_id,event_id)
+			VALUES (11,14,3);			
+INSERT INTO participants (id,user_id,event_id)
+			VALUES (12,15,1);			
+INSERT INTO participants (id,user_id,event_id)
+			VALUES (13,16,2);			
+INSERT INTO participants (id,user_id,event_id)
+			VALUES (14,17,3);			
+INSERT INTO participants (id,user_id,event_id)
+			VALUES (15,18,2);			
+INSERT INTO participants (id,user_id,event_id)
+			VALUES (16,19,1);			
+INSERT INTO participants (id,user_id,event_id)
+			VALUES (17,20,3);			
+INSERT INTO participants (id,user_id,event_id)
+			VALUES (18,12,6);			
+INSERT INTO participants (id,user_id,event_id)
+			VALUES (19,12,5);			
+INSERT INTO participants (id,user_id,event_id)
+			VALUES (20,10,4);
+			
+			
+			
+INSERT INTO owners (id,user_id,event_id)
+			VALUES (1,1,1);
+INSERT INTO owners (id,user_id,event_id)
+			VALUES (2,2,2);			
+INSERT INTO owners (id,user_id,event_id)
+			VALUES (3,3,3);			
+INSERT INTO owners (id,user_id,event_id)
+			VALUES (4,3,4);			
+INSERT INTO owners (id,user_id,event_id)
+			VALUES (5,5,5);			
+INSERT INTO owners (id,user_id,event_id)
+			VALUES (6,1,6);			
+INSERT INTO owners (id,user_id,event_id)
+			VALUES (7,6,7);			
+INSERT INTO owners (id,user_id,event_id)
+			VALUES (8,12,8);
+
+			
+			
+INSERT INTO images (id,path) VALUES (1,'/imgs/natur.jpg');			
+INSERT INTO images (id,path) VALUES (2,'/imgs/natu.jpg');			
+INSERT INTO images (id,path) VALUES (3,'/imgs/pyr.jpg');			
+INSERT INTO images (id,path) VALUES (4,'/imgs/november.jpg');			
+INSERT INTO images (id,path) VALUES (5,'/imgs/taj.jpg');			
+INSERT INTO images (id,path) VALUES (6,'/imgs/fer.jpg');			
+INSERT INTO images (id,path) VALUES (7,'/imgs/fa1.jpg');			
+INSERT INTO images (id,path) VALUES (8,'/imgs/fa2.jpg');
+
+INSERT INTO localizations (id,name,address,latitude,longitude,city_id) VALUES (1,'Restaurante O Pirata','Rua da Isabelinha',41.452993,-8.5775364,1);
+INSERT INTO localizations (id,name,address,latitude,longitude,city_id) VALUES (2,'FEUP','Rua Roberto Frias',41.1779401,-8.5998763,2);
+INSERT INTO localizations (id,name,address,latitude,longitude,city_id) VALUES (3,'Parque da BelaVista','Av. Arlindo Vicente',38.7507558,-9.1265431,3);
+INSERT INTO localizations (id,name,address,latitude,longitude,city_id) VALUES (4,'Passeio Maritimo de Alges','Alges',38.697318,-9.2375993,3);
+INSERT INTO localizations (id,name,address,latitude,longitude,city_id) VALUES (5,'Hotel Douro','Rua de Agramonte',41.1564707,-8.6288115,2);
+INSERT INTO localizations (id,name,address,latitude,longitude,city_id) VALUES (6,'Norte shopping','Matosinhos',41.1825143,-8.6803795,2);
+
+
+INSERT INTO cities (id,name,country_id) VALUES (1,'Braga',1);
+INSERT INTO cities (id,name,country_id) VALUES (1,'Porto',1);
+INSERT INTO cities (id,name,country_id) VALUES (1,'Lisboa',1);
+
+INSERT INTO countries (id,name) VALUES (1,'Portugal');
+INSERT INTO countries (id,name) VALUES (2,'Espanha');
+INSERT INTO countries (id,name) VALUES (3,'USA');
+
+
+INSERT INTO post (id,name) VALUES (3,'USA');
+
+
+
+		
